d0bfb1d57b0518c61fba9bb627df40781a50aa56
muaimingjun <2667004988@qq.com>
2026-06-09 11:15:34 +0800

    Revert "chore: 迁移 API 地址到 qj.wanwuhuanxin.cn，更新联系方式"
    This reverts commit 03ffacb01c86ba9d04c835f03cb1885fde0e68fe.

--- js/ui/events.js
+++ js/ui/events.js
@@ -32,7 +32,7 @@ export class EventManager {
 
         if (this.dom.apiBaseUrl) {
             this.dom.apiBaseUrl.addEventListener('change', (e) => {
-                const rawUrl = (e.target.value || '').trim() || 'https://qj.wanwuhuanxin.cn';
+                const rawUrl = (e.target.value || '').trim() || 'https://api.wanwuhuanxin.cn';
                 const finalUrl = normalizeUrl(rawUrl);
                 e.target.value = finalUrl;
                 // 保存到 localStorage
